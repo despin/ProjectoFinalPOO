6cfd12906c12137724b3eb999a90cdd22f4b33e0
Pedro Calogero <sp200091@gmail.com>
2017-10-25 00:00:56 -0300

    pantalla del panel de control añadida

---
+++ src/FrontEnd/PanelControl.java
@@ -0,0 +1,12 @@
+package FrontEnd;
+
+import java.awt.Container;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class PanelControl extends JPanel {
+	PanelControl(JFrame marco, String codEmpleado) {
+		
+	}
+}
