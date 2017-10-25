362bd76f68418a12cbc2894f421701d6f2437378
Pedro Calogero <sp200091@gmail.com>
2017-10-24 23:41:14 -0300

    cartel de venta nueva añadido

--- src/FrontEnd/Inicio.java
+++ src/FrontEnd/Inicio.java
@@ -5,8 +5,11 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Inicio extends JPanel{
+	private JTextField codEmpleado;
 	Inicio (JFrame marco) {
 		setLayout(null);
 		
@@ -31,6 +34,15 @@ public class Inicio extends JPanel{
 		btnPanelControl.setMnemonic('c');
 		btnPanelControl.setBounds(107, 136, 230, 62);
 		add(btnPanelControl);
+		
+		JLabel lblCodigoDeEmpleado = new JLabel("Codigo de empleado");
+		lblCodigoDeEmpleado.setBounds(58, 35, 123, 15);
+		add(lblCodigoDeEmpleado);
+		
+		codEmpleado = new JTextField();
+		codEmpleado.setBounds(178, 33, 159, 19);
+		add(codEmpleado);
+		codEmpleado.setColumns(6);
 		
 	}
 }
---
+++ src/FrontEnd/VentaNueva.java
@@ -0,0 +1,54 @@
+package FrontEnd;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JScrollPane;
+import java.awt.GridLayout;
+import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JToolBar;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
+import javax.swing.JTable;
+
+public class VentaNueva extends JPanel {
+	private JTable table;
+	VentaNueva (JFrame marco, String codigoDeEmpleado) {
+		setLayout(new BorderLayout(0, 0));
+		
+		JButton btnAceptar = new JButton("Aceptar");
+		add(btnAceptar, BorderLayout.SOUTH);
+		
+		JPanel panel = new JPanel();
+		add(panel, BorderLayout.NORTH);
+		panel.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JButton btnAnadir = new JButton("Añadir ");
+		btnAnadir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel.add(btnAnadir);
+		
+		JButton btnQuitar = new JButton("Quitar");
+		panel.add(btnQuitar);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		add(scrollPane, BorderLayout.CENTER);
+		
+		JPanel panelScroll = new JPanel();
+		scrollPane.setViewportView(panelScroll);
+		panelScroll.setLayout(new BorderLayout(0, 0));
+		
+		table = new JTable();
+		panelScroll.add(table, BorderLayout.CENTER);
+		setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{panel, btnAnadir, btnQuitar, scrollPane, btnAceptar}));
+		
+	}
+}
