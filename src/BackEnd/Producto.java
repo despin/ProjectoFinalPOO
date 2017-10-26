9a671ce4c4b292c1d475211de8248153e6ac3fa1
despin <sp200091@gmail.com>
2017-10-26 14:39:24 -0300

    constructor nuevo a partir del codigo, y de la dbb

--- src/BackEnd/Producto.java
+++ src/BackEnd/Producto.java
@@ -2,19 +2,38 @@ package BackEnd;
 
 import java.util.ArrayList;
 
+import BackEndDAO.ProductoDAO;
+
 public class Producto {
 
-	int codigoBarras;
+	String codigoBarras;
 	String nombre;
 	int precio;
 	ArrayList <Descuento> descuentos = new ArrayList <Descuento>();
 
-	public Producto (int codigoBarras, String nombre, int precio){
-		
-		this.codigoBarras = codigoBarras;
+	public Producto (String codigo, String nombre, int precio){
+		this.codigoBarras = codigo;
 		this.nombre = nombre;
 		this.precio = precio;
-		
 	}
+	
+	public Producto Producto (String codigoBarras){
+		//Query para obtener el producto mediante un procedimiento almacenado
+		ProductoDAO dao = new ProductoDAO();
+		return dao.obtenerProductoDesdeCodigo(codigoBarras);
+	}
+	
+	public String getCodProducto() {
+		return this.codigoBarras;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public int getPrecio() {
+		return this.precio;
+	}
+	
 	
 }
