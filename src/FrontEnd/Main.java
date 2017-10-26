4f6d21cff016052c005a520d8efb27efe1d2d6fe
despin <sp200091@gmail.com>
2017-10-26 15:00:05 -0300

    tiempo de espera acortado, marco agrandado

--- src/FrontEnd/Main.java
+++ src/FrontEnd/Main.java
@@ -8,14 +8,14 @@ public class Main {
 		// TODO Auto-generated method stub
 		JFrame marco = new JFrame("SuperSoft v0.0 proto");
 		marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		marco.setBounds(10, 100, 400, 300);
+		marco.setBounds(10, 100, 1000, 400);
 		marco.setVisible(true);
 		
 		marco.setContentPane(new PantallaBienvenida(marco));
 		marco.validate();
 		
 		try {
-			Thread.sleep(1500);
+			Thread.sleep(100);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
