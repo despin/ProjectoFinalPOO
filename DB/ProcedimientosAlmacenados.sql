6b2323baf2fbce99dbad42916b1f0c00c0f41a37
unknown <ftoscano@L2Maq11.W2008R2L2.local>
2017-10-27 10:18:58 -0300

    Procedimientos de Venta, Empleado, Descuento y Producto

---
+++ DB/ProcedimientosAlmacenados.sql
@@ -0,0 +1,43 @@
+/** PRODUCTO **/
+
+DROP PROCEDURE IF EXISTS pa_producto;
+DELIMITER $$
+CREATE PROCEDURE pa_producto (IN _codigoBarras INT)
+BEGIN
+SELECT * FROM  Producto
+WHERE codigoBarras = codigoBarras;
+END $$
+DELIMITER ;
+
+/** VENTA **/
+
+DROP PROCEDURE IF EXISTS pa_venta;
+DELIMITER $$
+CREATE PROCEDURE pa_venta (IN _id_venta INT)
+BEGIN
+SELECT * FROM  Venta
+WHERE id_venta = id_venta;
+END $$
+DELIMITER ;
+
+/** DESCUENTO **/
+
+DROP PROCEDURE IF EXISTS pa_descuento;
+DELIMITER $$
+CREATE PROCEDURE pa_descuento (IN _id_descuento INT)
+BEGIN
+SELECT * FROM  Descuento
+WHERE id_descuento = id_descuento;
+END $$
+DELIMITER ;
+
+/** EMPLEADO **/
+
+DROP PROCEDURE IF EXISTS pa_empleado;
+DELIMITER $$
+CREATE PROCEDURE pa_empleado (IN _codEmpleado varchar(6))
+BEGIN
+SELECT * FROM  Empleado
+WHERE codEmpleado = codEmpleado;
+END $$
+DELIMITER ;
