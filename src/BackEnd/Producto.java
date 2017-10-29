1280317aba2cd55c4b1e28c7631bfd5fd505b963
despin <sp200091@gmail.com>
2017-10-29 02:37:16 -0300

    constructor estatico

--- src/BackEnd/Producto.java
+++ src/BackEnd/Producto.java
@@ -17,13 +17,15 @@ public class Producto {
 		this.precio = precio;
 	}
 	
-	public Producto (String codigoBarras){
+	public Producto (String codigoBarras) throws Exception {
 		//Query para obtener el producto mediante un procedimiento almacenado
-		ProductoDAO dao = new ProductoDAO();
+		/*ProductoDAO dao = new ProductoDAO();
 		Producto nuevo = dao.obtenerProductoDesdeCodigo(codigoBarras);
 		this.codigoBarras = nuevo.getCodProducto();
 		this.nombre = nuevo.getNombre();
-		this.precio = nuevo.getPrecio();
+		this.precio = nuevo.getPrecio();*/
+		this.nombre = "Producto_"+codigoBarras;
+		this.precio = 2;
 	}
 	
 	public String getCodProducto() {
