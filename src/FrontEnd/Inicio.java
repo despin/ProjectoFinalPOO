993d82e4dd771c6e4b7ca52239fc2ab2e8207e93
despin <sp200091@gmail.com>
2017-10-29 03:12:43 -0300

    implemento diseño responsivo

--- src/FrontEnd/Inicio.java
+++ src/FrontEnd/Inicio.java
@@ -7,13 +7,34 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JMenuBar;
 
 public class Inicio extends JPanel{
 	private JTextField codEmpleado;
 	Inicio (JFrame marco) {
-		setLayout(null);
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		add(panel, BorderLayout.NORTH);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JMenuBar menuBar = new JMenuBar();
+		panel.add(menuBar, BorderLayout.NORTH);
+		
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1);
+		
+		JLabel lblCodigoDeEmpleado = new JLabel("Codigo de empleado");
+		panel_1.add(lblCodigoDeEmpleado);
+		
+		codEmpleado = new JTextField();
+		panel_1.add(codEmpleado);
+		codEmpleado.setColumns(6);
 		
 		JButton btnVentaNueva = new JButton("Venta Nueva");
+		add(btnVentaNueva, BorderLayout.CENTER);
 		btnVentaNueva.setToolTipText("Inicia el proceso de venta");
 		btnVentaNueva.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -22,10 +43,9 @@ public class Inicio extends JPanel{
 			}
 		});
 		btnVentaNueva.setMnemonic('v');
-		btnVentaNueva.setBounds(107, 62, 230, 62);
-		add(btnVentaNueva);
 		
 		JButton btnPanelControl = new JButton("Panel de control");
+		add(btnPanelControl, BorderLayout.SOUTH);
 		btnPanelControl.setToolTipText("Administra y monitoriza las transacciones del sistema");
 		btnPanelControl.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -34,17 +54,6 @@ public class Inicio extends JPanel{
 			}
 		});
 		btnPanelControl.setMnemonic('c');
-		btnPanelControl.setBounds(107, 136, 230, 62);
-		add(btnPanelControl);
-		
-		JLabel lblCodigoDeEmpleado = new JLabel("Codigo de empleado");
-		lblCodigoDeEmpleado.setBounds(58, 35, 123, 15);
-		add(lblCodigoDeEmpleado);
-		
-		codEmpleado = new JTextField();
-		codEmpleado.setBounds(178, 33, 159, 19);
-		add(codEmpleado);
-		codEmpleado.setColumns(6);
 		
 	}
 }
