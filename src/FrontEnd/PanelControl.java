38118bf4e109ecc430844e1d5c23709e76f6cbcb
despin <sp200091@gmail.com>
2017-10-29 17:54:43 -0300

    corregido para que concuende con Inicio

--- src/FrontEnd/PanelControl.java
+++ src/FrontEnd/PanelControl.java
@@ -5,8 +5,10 @@ import java.awt.Container;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import BackEnd.Empleado;
+
 public class PanelControl extends JPanel {
-	PanelControl(JFrame marco, String codEmpleado) {
+	PanelControl(JFrame marco,Empleado empleado) {
 		
 	}
 }
