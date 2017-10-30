a2b828fa5d245dc9835353152a976d84d0d7aef9
despin <sp200091@gmail.com>
2017-10-30 10:51:35 -0300

    añadida disposicion adaptiva

--- src/FrontEnd/PantallaBienvenida.java
+++ src/FrontEnd/PantallaBienvenida.java
@@ -6,25 +6,24 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
 
 public class PantallaBienvenida extends JPanel {
 	public PantallaBienvenida(JFrame marco) {
-		setLayout(null);
+		setLayout(new BorderLayout(0, 0));
 		
-		JLabel lblNewLabel = new JLabel("Hola, bienvenido a MarketSystem");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(lblNewLabel.getFont().deriveFont(lblNewLabel.getFont().getSize() + 6f));
-		lblNewLabel.setBounds(40, 27, 357, 23);
-		add(lblNewLabel);
+		JLabel lblBienvenidoAMarketsystem = new JLabel("Bienvenido a MarketSystem");
+		lblBienvenidoAMarketsystem.setFont(new Font("Dialog", Font.BOLD, 20));
+		lblBienvenidoAMarketsystem.setHorizontalAlignment(SwingConstants.CENTER);
+		add(lblBienvenidoAMarketsystem, BorderLayout.NORTH);
 		
 		JProgressBar progressBar = new JProgressBar();
 		progressBar.setIndeterminate(true);
-		progressBar.setBounds(31, 168, 348, 14);
-		add(progressBar);
+		add(progressBar, BorderLayout.SOUTH);
 		
 		JLabel lblCargando = new JLabel("Cargando...");
-		lblCargando.setBounds(158, 141, 111, 15);
-		add(lblCargando);
+		lblCargando.setHorizontalAlignment(SwingConstants.CENTER);
+		add(lblCargando, BorderLayout.CENTER);
 		
 
 	}
