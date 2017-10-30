58a2b3a848ff1186486ce29a4220625b89bc66bb
despin <sp200091@gmail.com>
2017-10-30 15:52:35 -0300

    constructor a partir de clave añadido

--- src/BackEnd/Descuento.java
+++ src/BackEnd/Descuento.java
@@ -1,17 +1,28 @@
 package BackEnd;
 
+import BackEndDAO.DescuentoDAO;
 import java.util.ArrayList;
 
 public class Descuento {
 
 	String palabraClave;
 	int porcentaje;
+	ArrayList<Producto> productosAfectados;
 	
 	
 	public Descuento(String palabraClave, int porcentaje){
 		
 		this.palabraClave = palabraClave;
 		this.porcentaje = porcentaje;
+		
+	}
+
+	public Descuento(String palabraClave) {
+		DescuentoDAO dao = new DescuentoDAO();
+		Descuento nuevo = dao.obtenerDescuentoDesdeCodigo();
+		this.palabraClave = palabraClave;
+		this.porcentaje = nuevo.porcentaje;
+		ArrayList<Producto> afectados = new ArrayList<Producto>();
 		
 	}
 	
