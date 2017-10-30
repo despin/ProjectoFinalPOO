f09ad1c696c7e998497e69ffa6d2e3991d12bd4e
despin <sp200091@gmail.com>
2017-10-30 09:51:51 -0300

    el flujo se puede bifurcar

--- src/FrontEnd/CierreVenta.java
+++ src/FrontEnd/CierreVenta.java
@@ -1,45 +1,63 @@
 package FrontEnd;
 
 import javax.swing.JPanel;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
 
 import BackEnd.Empleado;
+import BackEnd.Venta;
 
 import javax.swing.JLabel;
+import java.awt.BorderLayout;
 import javax.swing.JMenuBar;
-
-import java.util.Vector;
-
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class CierreVenta extends JPanel {
-	public CierreVenta(JFrame marco, int totalAbonar, Empleado empleado) {
-		setLayout(null);
+	public CierreVenta(JFrame marco, Empleado empleado, Integer total) {
+		setLayout(new BorderLayout(0, 0));
 		
-		JLabel lblDebeAbonarxxx = new JLabel("Debe abonar $"+totalAbonar);
-		lblDebeAbonarxxx.setBounds(143, 67, 131, 15);
-		add(lblDebeAbonarxxx);
-		
-		JLabel lblGr = new JLabel("Gracias por comprar con MarketSystem");
-		lblGr.setBounds(74, 121, 303, 15);
-		add(lblGr);
+		JPanel panel = new JPanel();
+		add(panel, BorderLayout.NORTH);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
 		
 		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBounds(0, 0, 438, 21);
-		add(menuBar);
+		panel.add(menuBar);
+		
+		JPanel panel_1 = new JPanel();
+		add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(null);
+		
+		JLabel label_1 = new JLabel("GRACIAS POR COMPRAR");
+		label_1.setBounds(12, 85, 329, 15);
+		panel_1.add(label_1);
 		
 		JButton btnVolver = new JButton("Volver");
+		
+		btnVolver.setBounds(38, 166, 117, 25);
+		panel_1.add(btnVolver);
+		
+		JLabel lblTotalX = new JLabel("Total: "+total.toString());
+		lblTotalX.setBounds(39, 119, 123, 15);
+		panel_1.add(lblTotalX);
+		
+		JButton btnIrAlInicio = new JButton("Ir al inicio");
+		btnIrAlInicio.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				marco.setContentPane(new Inicio(marco));
+				marco.validate();
+			}
+		});
+		btnIrAlInicio.setBounds(167, 166, 117, 25);
+		panel_1.add(btnIrAlInicio);
+		
 		btnVolver.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				marco.setContentPane(new VentaNueva(marco, empleado));
+				marco.validate();
 			}
 		});
-		btnVolver.setBounds(128, 233, 117, 25);
-		add(btnVolver);
-		
 	}
 }
