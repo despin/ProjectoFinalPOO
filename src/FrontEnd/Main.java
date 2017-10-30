535046682e75543886ada011c92d84f113db0575
despin <sp200091@gmail.com>
2017-10-30 10:52:02 -0300

    tiempo de espera aumentado

--- src/FrontEnd/Main.java
+++ src/FrontEnd/Main.java
@@ -8,14 +8,14 @@ public class Main {
 		// TODO Auto-generated method stub
 		JFrame marco = new JFrame("SuperSoft v0.0 proto");
 		marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		marco.setBounds(10, 100, 1000, 400);
+		marco.setBounds(300, 100, 800, 600);
 		marco.setVisible(true);
 		
 		marco.setContentPane(new PantallaBienvenida(marco));
 		marco.validate();
 		
 		try {
-			Thread.sleep(100);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
