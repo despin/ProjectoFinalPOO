b83d2179997e48a2cd2db131741266835b1a5902
despin <sp200091@gmail.com>
2017-11-01 20:11:20 -0300

    me olvide de agregar estos backendDAO :(

---
+++ src/BackEndDAO/ProductoVendidoDAO.java
@@ -0,0 +1,44 @@
+package BackEndDAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import BackEnd.ProductoVendido;
+import BackEnd.Venta;
+
+public class ProductoVendidoDAO {
+
+	public ArrayList<ProductoVendido> obtenerProductosPorVenta(int id) throws SQLException {
+		// TODO Auto-generated method stub
+		Connection conexion = Conexion.conectar();
+		Statement declaracion = conexion.createStatement();
+		PreparedStatement prepared = conexion.prepareStatement("Select * From ItemProducto where id_Venta = ?");
+		prepared.setInt(1, id);
+		ResultSet rsInterno = prepared.executeQuery();
+		return null;
+	}
+	
+	public boolean insertar(ProductoVendido productoVendido, int idVenta) {
+		try {
+			Connection conexion = Conexion.conectar();
+			Statement declaracion = conexion.createStatement();
+			PreparedStatement prepared = conexion.prepareStatement("INSERT INTO ItemProducto VALUES ( default , ?, ?, ?, ? );");
+			prepared.setInt(1, idVenta);
+			prepared.setString(2, productoVendido.getProducto().getCodProducto());
+			prepared.setInt(3, productoVendido.getCantidad());
+			prepared.setInt(4, productoVendido.getCantidad()*productoVendido.getProducto().getPrecio());
+			prepared.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+}
---
+++ src/BackEndDAO/VentaDAO.java
@@ -0,0 +1,71 @@
+package BackEndDAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+
+import BackEnd.Descuento;
+import BackEnd.Empleado;
+import BackEnd.Venta;
+
+public class VentaDAO {
+	public VentaDAO() {
+		
+	}
+
+	public ArrayList<Venta> obtenerTodosLasVentas() throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		ArrayList<Venta> ventas = new ArrayList<Venta>();
+		Connection conexion = Conexion.conectar();
+		Statement declaracion = conexion.createStatement();
+		PreparedStatement prepared = conexion.prepareStatement("Select * From Venta");
+		ResultSet rsInterno = prepared.executeQuery();
+		
+		while (rsInterno.next()) {
+			ventas.add(new Venta(
+					rsInterno.getInt("Id_Venta"),
+					new Empleado(rsInterno.getString("cajero")),
+					rsInterno.getDate("fechaVenta")
+			));
+			System.out.println(rsInterno.getDate("fechaVenta"));
+		}
+
+		return ventas;
+	}
+
+//Devuelve un Date de SQL
+	
+	public int insertar(Venta venta, int totalAbonar) throws SQLException {
+		// TODO Auto-generated method stub
+		long tiempo = venta.getFecha().getTime();
+		java.sql.Timestamp timestamp = new Timestamp(tiempo);
+		ArrayList<Venta> ventas = new ArrayList<Venta>();
+		Connection conexion = Conexion.conectar();
+		Statement declaracion = conexion.createStatement();
+		PreparedStatement prepared = conexion.prepareStatement("INSERT INTO Venta VALUES ( default , ? , ?, ? );");
+		prepared.setString(1, venta.getEmpleado().getCodigo());
+		prepared.setTimestamp(2, timestamp);
+		prepared.setInt(3, totalAbonar);
+		prepared.executeUpdate();
+		
+		System.out.println("timestamp "+tiempo);
+
+		
+		int ID = 0;
+		
+		prepared = conexion.prepareStatement("SELECT MAX(Venta.Id_Venta) FROM Venta;");
+		ResultSet rsInterno = prepared.executeQuery();
+		
+		while (rsInterno.next()) {
+			ID = rsInterno.getInt(1);
+		}
+		
+		System.out.println("EXEC"+ID);
+		return ID;
+	}
+}
