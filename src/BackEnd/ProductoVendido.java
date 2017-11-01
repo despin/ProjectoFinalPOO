ac51d631b1465c0d37cc83ed4e7c109bc5443d5e
despin <sp200091@gmail.com>
2017-11-01 20:14:17 -0300

    precio eliminado por reduncancia

--- src/BackEnd/ProductoVendido.java
+++ src/BackEnd/ProductoVendido.java
@@ -3,15 +3,11 @@ package BackEnd;
 public class ProductoVendido {
 	
 	Producto producto;
-	int precio;
 	int cantidad;
-	
-	
-	public ProductoVendido(int precio, int cantidad){
-		
-		this.precio = precio;
+
+	public ProductoVendido(Producto producto, int cantidad){
+		this.producto = producto;
 		this.cantidad = cantidad;
-		
 	}
 
 }
